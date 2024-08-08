bfe0591c580595b3772031200e8e658f20d1f20f
Akshatha . <akshat14@in.ibm.com>
2024-08-08 10:44:00 +0530

    Added requirement.txt file

--- file_upload_func.py
+++ file_upload_func.py
@@ -4,18 +4,22 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 def file_upload(filename,config,upload_to):
-    if upload_to.lower()=='google_drive':
-        gauth = GoogleAuth()
-        gauth.LocalWebserverAuth()
-        drive = GoogleDrive(gauth)
-        file = drive.CreateFile()
-        file.SetContentFile(filename)
-        file.Upload()
+    try:
+        if upload_to.lower()=='google_drive':
+            gauth = GoogleAuth()
+            gauth.LocalWebserverAuth()
+            drive = GoogleDrive(gauth)
+            file = drive.CreateFile()
+            file.SetContentFile(filename)
+            file.Upload()
 
-    elif upload_to.lower()=='s3':
-        s3_bucket=config['s3_bucket_name']
-        region=config['s3_region']
-        s3 = boto3.client('s3',region=region)
-        object_name = os.path.basename(filename)
-        s3.upload_file(filename,s3_bucket ,object_name)
+        elif upload_to.lower()=='s3':
+            s3_bucket=config['s3_bucket_name']
+            region=config['s3_region']
+            s3 = boto3.client('s3',region=region)
+            object_name = os.path.basename(filename)
+            s3.upload_file(filename,s3_bucket ,object_name)
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        raise
 
--- sacumen_py_assignment.py
+++ sacumen_py_assignment.py
@@ -1,17 +1,17 @@
 
 import json
 import os
-base_file_directory  = os.getcwd()
 from file_upload_func import file_upload
+base_file_directory  = os.getcwd()
 
 def classify_file(dirpath,filename,config):
     if os.path.splitext(filename)[1] in config['file_type_for_s3']:
         print(f"The {filename} will be uploaded to S3")
-        #file_upload(dirpath+"\\"+filename,config,"s3")
+        file_upload(dirpath+"\\"+filename,config,"s3")
         return "push to s3"
     elif os.path.splitext(filename)[1] in config['file_type_for_GD']:
         print(f"The {filename} will be uploaded to google drive")
-        #file_upload(dirpath+"\\"+filename,config,"google_drive")
+        file_upload(dirpath+"\\"+filename,config,"google_drive")
         return "push to google drive"
     else:
         print(f"The {filename} will be not pushed to any of the cloud storage")
@@ -19,7 +19,6 @@ def classify_file(dirpath,filename,config):
     
 def read_all_files(file_dir):
     try:
-
         config=read_config(base_file_directory)
         if config.get('input_file_directory'):
             file_dir=config.get('input_file_directory')
@@ -27,7 +26,6 @@ def read_all_files(file_dir):
         for (dirpath, dirnames, filenames) in w:
             for f in filenames:
                 response=classify_file(dirpath,f,config)
-
         return response
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
